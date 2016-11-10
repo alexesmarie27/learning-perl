4dbda787dbb8d131ce0da3696af62cddc8da014c
Alexes <ampress727@yahoo.com>
2016-11-09 21:00:32 -0600

    added comments

--- datechecker.pl
+++ datechecker.pl
@@ -6,10 +6,18 @@
 use strict;
 use warnings;
 
+# checks for correct number of command line arguments
 ($#ARGV == 0) or die "Usage: $0 [date]\n";
 
+# gets the date from the command line
 my $string = shift @ARGV;
 
+# checks for dates of the forms: 
+## YYYY-MM-DD
+## MM/DD/YYYY
+## MM/DD/YY
+## MON DD, YYYY
+# if the form is correct, the date is printed to the user
 if($string =~ /(\d\d\d\d)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/){
 	print $string, " is a valid date!\n";
 	print "The year is ", $1 if defined $1;
