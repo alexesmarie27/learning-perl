7923ea6b4157e8b6bd40bb645ef884a3bdd49606
Alexes <ampress727@yahoo.com>
2016-11-09 19:29:39 -0600

    Initial file creation

---
+++ datechecker.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+($#ARGV == 0) or die "Usage: $0 [date]\n";
+
+my $string = shift @ARGV;
+
+if($string =~ ){
+
+}
+else{
+	print $string, " is not a valid date\n";
+}
+
